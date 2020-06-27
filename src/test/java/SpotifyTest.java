ad2aa23de79ea701e516a315ca4a72ff66cc784b
rishibodake <rishibodake@gmail.com>
2020-06-27 12:49:52 +0530

    Refactored: Removed Unnecesary Test Cases And Merged Them In One

--- src/test/java/SpotifyTest.java
+++ src/test/java/SpotifyTest.java
@@ -19,9 +19,8 @@ public class SpotifyTest {
     //Before method
     @BeforeMethod
     public void get() {
-        token = "Bearer ";//Latest Token
+        token = "Bearer ";
     }
-
 
     @Test(priority = 1)
     public void userID_GET_Request() {
@@ -32,22 +31,10 @@ public class SpotifyTest {
                 .when()
                 .get("https://api.spotify.com/v1/me");
         user_ID = response.path("id");//This step will fetch the user id
+        String userName = response.path("display_name");
+        System.out.println(userName);
         response.then().assertThat().statusCode(200); //Validation of status code
         //System.out.println(user_ID);//optional
-    }
-
-
-    @Test(priority = 2)
-    public void userName_GET_Request() {
-        Response response = given()
-                .contentType(JSON)
-                .accept(JSON)
-                .header("Authorization", token)
-                .when()
-                .get("https://api.spotify.com/v1/me");
-        String userName = response.path("display_name"); //this step will fetch the user name
-        response.then().assertThat().statusCode(200); //Validation of Status code
-        //System.out.println(userName);//Optional
     }
 
 
@@ -63,7 +50,6 @@ public class SpotifyTest {
        // response.prettyPrint(); //print current user profile //optional
     }
 
-
     @Test(priority = 4)
     public void playListNumber_GET_Request() {   //get number of total playlist present
         Response response = given()
@@ -74,10 +60,9 @@ public class SpotifyTest {
                 .get("https://api.spotify.com/v1/users/" + user_ID + "/playlists");
         response.then().assertThat().statusCode(200);
         totalNoOfPlayList = response.path("total"); //get total playlist
-        System.out.println("Total PlayList:" + totalNoOfPlayList);
+        System.out.println("Total PlayList:" + totalNoOfPlayList);//optional
         //response.prettyPrint(); //Optional
     }
-
 
     @Test(priority = 5)
     public void userPlayListInfo_GET_Request() { //get details like UserId of each playlist created
@@ -92,11 +77,12 @@ public class SpotifyTest {
         for (int index = 0; index < playListArray.length; index++) {
             playListArray[index] = response.path("items[" + index + "].id"); //get playlist id
         }
-      /*  for (String id : playList) { //print play list
+        /*for (String id : playListArray) { //print play list
             System.out.println("PlayList Id" + id);
-        }*/             //optional
+        }
+                   //optional
+         */
     }
-
 
     @Test(priority = 6)//10
     public void createPlayList_POST_Request() {
